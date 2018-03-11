9662fafad0fee17e4b07e2010d7a6b735f65185c
David Barth <davidbarth80@gmail.com>
2018-03-11 13:57:37 +0000

    Add class async class library and WinForm projects

---
+++ BelfastAsync/BelfastSocketServer.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace BelfastAsync
+{
+    public class BelfastSocketServer
+    {
+        IPAddress myIP;
+        int myPort;
+        //helper class
+        TcpListener myTCPListener;
+
+        public async void StartListeningFoIncommingConnection(IPAddress ipAddress = null, int port = 23000)
+        {
+            //sanity check
+            if(ipAddress == null)
+            {
+                ipAddress = IPAddress.Any;
+            }
+
+            if(port <= 0)
+            {
+                port = 23000;
+            }
+
+            myIP = ipAddress;
+            myPort = port;
+
+            System.Diagnostics.Debug.WriteLine(string.Format("IP Adress: {0} - Port: {1}", myIP.ToString(), myPort.ToString()));
+
+            myTCPListener = new TcpListener(myIP, myPort);
+            myTCPListener.Start();
+
+            var returnedByAccept =  await myTCPListener.AcceptTcpClientAsync(); //returns a TCPClient (helper class)
+
+            System.Diagnostics.Debug.WriteLine("Client connected successfully: " + returnedByAccept.ToString());
+        }
+    }
+}
