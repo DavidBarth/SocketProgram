33019525ad8e85f4d66a8cfde9ee756ebddd0094
David Barth <davidbarth80@gmail.com>
2018-04-09 22:02:57 +0100

    Add AsyncSocketServer

---
+++ AsyncSocketServer/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WinFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAccceptAsync = new System.Windows.Forms.Button();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.sendAllButton = new System.Windows.Forms.Button();
+            this.stopServerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonAccceptAsync
+            // 
+            this.buttonAccceptAsync.Location = new System.Drawing.Point(29, 12);
+            this.buttonAccceptAsync.Name = "buttonAccceptAsync";
+            this.buttonAccceptAsync.Size = new System.Drawing.Size(233, 23);
+            this.buttonAccceptAsync.TabIndex = 0;
+            this.buttonAccceptAsync.Text = "Accept Incoming Connection ";
+            this.buttonAccceptAsync.UseVisualStyleBackColor = true;
+            this.buttonAccceptAsync.Click += new System.EventHandler(this.buttonAccceptAsync_Click);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(26, 52);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(53, 13);
+            this.messageLabel.TabIndex = 1;
+            this.messageLabel.Text = "Message:";
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(83, 52);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(179, 20);
+            this.messageTextBox.TabIndex = 2;
+            // 
+            // sendAllButton
+            // 
+            this.sendAllButton.Location = new System.Drawing.Point(187, 93);
+            this.sendAllButton.Name = "sendAllButton";
+            this.sendAllButton.Size = new System.Drawing.Size(75, 23);
+            this.sendAllButton.TabIndex = 3;
+            this.sendAllButton.Text = "Send All";
+            this.sendAllButton.UseVisualStyleBackColor = true;
+            this.sendAllButton.Click += new System.EventHandler(this.sendAllButton_Click);
+            // 
+            // stopServerButton
+            // 
+            this.stopServerButton.Location = new System.Drawing.Point(29, 218);
+            this.stopServerButton.Name = "stopServerButton";
+            this.stopServerButton.Size = new System.Drawing.Size(75, 23);
+            this.stopServerButton.TabIndex = 4;
+            this.stopServerButton.Text = "Stop Server";
+            this.stopServerButton.UseVisualStyleBackColor = true;
+            this.stopServerButton.Click += new System.EventHandler(this.stopServerButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.stopServerButton);
+            this.Controls.Add(this.sendAllButton);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.buttonAccceptAsync);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAccceptAsync;
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Button sendAllButton;
+        private System.Windows.Forms.Button stopServerButton;
+    }
+}
+
---
+++ AsyncSocketServer/Form1.cs
@@ -0,0 +1,36 @@
+﻿using BelfastSocketAsync;
+using System;
+using System.Windows.Forms;
+
+namespace WinFormApp
+{
+    public partial class Form1 : Form
+    {
+        BelfastSocketServer server;
+        public Form1()
+        {
+            InitializeComponent();
+            server = new BelfastSocketServer();
+        }
+
+        private void buttonAccceptAsync_Click(object sender, EventArgs e)
+        {            
+            server.StartListeningFoIncommingConnection();
+        }
+
+        private void sendAllButton_Click(object sender, EventArgs e)
+        {
+            server.SendToAllClients(messageTextBox.Text);
+        }
+
+        private void stopServerButton_Click(object sender, EventArgs e)
+        {
+            server.StopServer();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            server.StopServer();
+        }
+    }
+}
